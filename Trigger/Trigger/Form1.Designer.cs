ae2c41b808dc27692d247ee4d3761849976fc0bf
MatteoTible <71816381+MatteoTible@users.noreply.github.com>
2021-02-17 23:15:35 +0100

    Trigger

---
+++ Trigger/Trigger/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Trigger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dgvStoricoAggiornamenti = new System.Windows.Forms.DataGridView();
+            this.dgvStoricoCancellazioni = new System.Windows.Forms.DataGridView();
+            this.dgvClienti = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoAggiornamenti)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoCancellazioni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClienti)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(13, 206);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(203, 28);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Aggiorna i dati su DB";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // dgvStoricoAggiornamenti
+            // 
+            this.dgvStoricoAggiornamenti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStoricoAggiornamenti.Location = new System.Drawing.Point(13, 242);
+            this.dgvStoricoAggiornamenti.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvStoricoAggiornamenti.Name = "dgvStoricoAggiornamenti";
+            this.dgvStoricoAggiornamenti.Size = new System.Drawing.Size(838, 185);
+            this.dgvStoricoAggiornamenti.TabIndex = 6;
+            // 
+            // dgvStoricoCancellazioni
+            // 
+            this.dgvStoricoCancellazioni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStoricoCancellazioni.Location = new System.Drawing.Point(531, 13);
+            this.dgvStoricoCancellazioni.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvStoricoCancellazioni.Name = "dgvStoricoCancellazioni";
+            this.dgvStoricoCancellazioni.Size = new System.Drawing.Size(320, 185);
+            this.dgvStoricoCancellazioni.TabIndex = 5;
+            // 
+            // dgvClienti
+            // 
+            this.dgvClienti.AllowUserToAddRows = false;
+            this.dgvClienti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClienti.Location = new System.Drawing.Point(13, 13);
+            this.dgvClienti.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvClienti.Name = "dgvClienti";
+            this.dgvClienti.RowHeadersVisible = false;
+            this.dgvClienti.Size = new System.Drawing.Size(489, 185);
+            this.dgvClienti.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 461);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dgvStoricoAggiornamenti);
+            this.Controls.Add(this.dgvStoricoCancellazioni);
+            this.Controls.Add(this.dgvClienti);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoAggiornamenti)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoCancellazioni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClienti)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridView dgvStoricoAggiornamenti;
+        private System.Windows.Forms.DataGridView dgvStoricoCancellazioni;
+        private System.Windows.Forms.DataGridView dgvClienti;
+    }
+}
+
---
+++ Trigger/Trigger/SQLQuery2.sql
@@ -0,0 +1,40 @@
+﻿CREATE TRIGGER UpdateClienti
+	ON [dbo].[Clienti]
+	AFTER UPDATE
+	AS
+	BEGIN
+		DECLARE @IdCliente AS INT;
+		DECLARE @NomeOld AS VARCHAR(50);
+		DECLARE @NomeNew AS VARCHAR(50);
+		DECLARE @CognomeOld AS VARCHAR(50);
+		DECLARE @CognomeNew AS VARCHAR(50);
+		DECLARE @IdCarrelloOld AS VARCHAR(50);
+		DECLARE @IdCarrelloNew AS VARCHAR(50);
+
+		SET @IdCliente = OLD.IdCliente;
+		SET @NomeOld = OLD.Nome;
+		SET @NomeNew = NEW.Nome;
+		SET @CognomeOld = OLD.Cognome;
+		SET @CognomeNew = NEW.Cognome;
+		SET @IdCarrelloOld = OLD.IdCarrello;
+		SET @IdCarrelloNew = NEW.IdCarrello;
+
+		IF (@NomeOld = @NomeNew)
+			SET @NomeOld = NULL;
+		IF (@NomeOld = NULL)
+			SET @NomeNew = NULL;
+
+		IF (@CognomeOld = @CognomeNew)
+			SET @CognomeOld = NULL;
+		IF (@CognomeOld = NULL)
+			SET @CognomeNew = NULL;
+		
+		IF (@IdCarrelloOld = @IdCarrelloNew)
+			SET @IdCarrelloOld = NULL;
+		IF (@IdCarrelloOld = NULL)
+			SET @IdCarrelloNew = NULL;
+		
+
+		INSERT INTO StoricoAggiornamenti (IdCliente,NomeOld,NomeNew,CognomeOld,CognomeNew,IdCarrelloOld,IdCarrelloNew)
+		VALUES (@IdCliente,@NomeOld,@NomeNew,@CognomeOld,@CognomeNew,@IdCarrelloOld,@IdCarrelloNew);
+	END;
